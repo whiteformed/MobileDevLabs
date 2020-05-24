66327270492c3c2da6b72072e255b1bc07024a21
whiteformed <whiteformed@gmail.com>
2020-05-24 18:08:23 +0300

    added Edit feature to Saved Countries Fragment

--- app/src/main/java/com/example/mobiledevlabs/FragmentChooseCountry.java
+++ app/src/main/java/com/example/mobiledevlabs/FragmentChooseCountry.java
@@ -1,6 +1,5 @@
 package com.example.mobiledevlabs;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.os.Bundle;
 
--- app/src/main/java/com/example/mobiledevlabs/FragmentSavedCountries.java
+++ app/src/main/java/com/example/mobiledevlabs/FragmentSavedCountries.java
@@ -1,5 +1,6 @@
 package com.example.mobiledevlabs;
 
+import android.app.Dialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,12 +12,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import static androidx.constraintlayout.widget.Constraints.TAG;
-
 
 public class FragmentSavedCountries extends Fragment implements RecyclerViewItemClickListener {
     private RecyclerViewAdapter adapter;
@@ -27,10 +32,10 @@ public class FragmentSavedCountries extends Fragment implements RecyclerViewItem
 
     @Override
     public void onDeleteItemButtonClicked(int pos) {
-        boolean res = sqlDBHelper.deleteData(tableSavedCountries, savedCountriesArrayList.get(pos));
-        performTask(1);
+        boolean success = sqlDBHelper.deleteData(tableSavedCountries, savedCountriesArrayList.get(pos));
+        accessDatabase(1);
 
-        if (res) {
+        if (success) {
             Log.i(TAG, "onDeleteItemButtonClicked: Successfully deleted " + savedCountriesArrayList.get(pos).getName() + " from DB");
             Toast.makeText(getActivity(), "Successfully deleted", Toast.LENGTH_SHORT).show();
         } else {
@@ -41,7 +46,57 @@ public class FragmentSavedCountries extends Fragment implements RecyclerViewItem
 
     @Override
     public void onUpdateItemButtonClicked(int pos) {
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.setContentView(R.layout.dialog_new_item);
+        dialog.setCancelable(true);
 
+        TextView tv_message = dialog.findViewById(R.id.message_tv);
+        tv_message.setText(R.string.message_update);
+
+        final Country oldCountry = savedCountriesArrayList.get(pos);
+
+        final EditText et_country = dialog.findViewById(R.id.et_country);
+        final EditText et_capital = dialog.findViewById(R.id.et_capital);
+        final EditText et_square = dialog.findViewById(R.id.et_square);
+
+        et_country.setText(oldCountry.getName());
+        et_capital.setText(oldCountry.getCapital());
+        et_square.setText(oldCountry.getSquare());
+
+        final Animation animAlpha = AnimationUtils.loadAnimation(dialog.getContext(), R.anim.anim_button_alpha);
+        Button button_confirm = dialog.findViewById(R.id.button_confirm);
+        button_confirm.setText(R.string.button_text_update);
+
+        View.OnClickListener onButtonUpdateClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (et_country.getText().toString().equals("") || et_capital.getText().toString().equals("") || et_square.getText().toString().equals("")) {
+                    Toast.makeText(getActivity(), "No empty fields allowed!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    v.startAnimation(animAlpha);
+                    Country newCountry = new Country(et_country.getText().toString(), et_capital.getText().toString(), et_square.getText().toString());
+
+                    boolean success = sqlDBHelper.updateData(tableSavedCountries, oldCountry, newCountry);
+                    accessDatabase(1);
+
+                    if (success) {
+                        Log.i(TAG, "onUpdateItemButtonClicked: Successfully updated " + oldCountry.getName() + " to " + newCountry.getName() + " at DB");
+                        Toast.makeText(getActivity(), "Successfully updated", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Log.i(TAG, "onUpdateItemButtonClicked: Failed updating " + oldCountry.getName() + " to " + newCountry.getName() + " at DB");
+                        Toast.makeText(getActivity(), "Updating failed", Toast.LENGTH_SHORT).show();
+                    }
+
+                    dialog.cancel();
+                }
+            }
+        };
+
+        button_confirm.setOnClickListener(onButtonUpdateClickListener);
+
+        dialog.show();
     }
 
     @Override
@@ -64,13 +119,13 @@ public class FragmentSavedCountries extends Fragment implements RecyclerViewItem
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-        performTask(1); // get
+        accessDatabase(1); // get
 
         SwipeRefreshLayout.OnRefreshListener onRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 swipeRefreshLayout.setRefreshing(false);
-                performTask(1);
+                accessDatabase(1);
                 adapter.notifyDataSetChanged();
             }
         };
@@ -80,7 +135,7 @@ public class FragmentSavedCountries extends Fragment implements RecyclerViewItem
         return view;
     }
 
-    private void performTask(int i) {
+    private void accessDatabase(int i) {
         Integer[] ops = {0, 1};
         AsynchronousTask accessDatabase = new AsynchronousTask(adapter, savedCountriesArrayList, sqlDBHelper, tableSavedCountries);
         accessDatabase.execute(ops[i]);
